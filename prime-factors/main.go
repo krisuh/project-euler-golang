788bf38306a89656968686c6f67c00882f36a187
Kristian <kristian.heikinpoika@gmail.com>
2018-05-27 12:30:22 +0300

    Fixed is prime algorithm.

--- prime-factors/main.go
+++ prime-factors/main.go
@@ -22,7 +22,7 @@ func isPrime(n int) bool {
 	} else if n%2 == 0 || n%3 == 0 {
 		return false
 	}
-	for i := 5; i*i < n; {
+	for i := 5; i*i <= n; {
 		if n%i == 0 || n%(i+2) == 0 {
 			return false
 		}
